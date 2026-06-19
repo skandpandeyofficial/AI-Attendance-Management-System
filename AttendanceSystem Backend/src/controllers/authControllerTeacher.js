3ff9243e9fd0fb0ebdd411456a064d222e178b82
skandpandeyofficial <2017skandpandey@gmail.com>
2026-06-20 02:08:15 +0530

    fix cross domain cookies

--- AttendanceSystem Backend/src/controllers/authControllerTeacher.js
+++ AttendanceSystem Backend/src/controllers/authControllerTeacher.js
@@ -29,7 +29,6 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
