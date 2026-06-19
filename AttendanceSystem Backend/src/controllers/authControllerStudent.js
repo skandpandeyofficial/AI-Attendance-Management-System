4de5277f6c3f4216e08cde2b208cc5742aadb5a7
skandpandeyofficial <2017skandpandey@gmail.com>
2026-06-20 02:36:28 +0530

    fix student auth cookie

--- AttendanceSystem Backend/src/controllers/authControllerStudent.js
+++ AttendanceSystem Backend/src/controllers/authControllerStudent.js
@@ -87,7 +87,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
